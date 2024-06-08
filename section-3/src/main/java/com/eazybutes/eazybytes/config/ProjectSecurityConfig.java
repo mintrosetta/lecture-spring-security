d83ab9c19bde8cf275223f3dd1105812f59b4803
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-08 18:59:19 +0700

    connect to mysql

--- section-3/src/main/java/com/eazybutes/eazybytes/config/ProjectSecurityConfig.java
+++ section-3/src/main/java/com/eazybutes/eazybytes/config/ProjectSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.eazybutes.eazybytes.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // tell spring boot, we configuration some data in this class
@@ -23,20 +26,30 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    // {noop} - NoOpPasswordEncoder store password with plain text
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withUsername("admin")
-            .password("{noop}admin")
-            .authorities("ADMIN")
-            .build();
-
-        UserDetails user = User.withUsername("user")
-            .password("{noop}user")
-            .authorities("USER")
-            .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // {noop} - NoOpPasswordEncoder store password with plain text
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsService() {
+    //     UserDetails admin = User.withUsername("admin")
+    //         .password("{noop}admin")
+    //         .authorities("ADMIN")
+    //         .build();
+
+    //     UserDetails user = User.withUsername("user")
+    //         .password("{noop}user")
+    //         .authorities("USER")
+    //         .build();
+
+    //     return new InMemoryUserDetailsManager(admin, user);
+    // }
 
 }
