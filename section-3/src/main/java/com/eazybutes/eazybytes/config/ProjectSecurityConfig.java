ff55d1a7987f98574a202ce866007bc21c65cd56
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-08 12:46:25 +0700

    add default password

--- section-3/src/main/java/com/eazybutes/eazybytes/config/ProjectSecurityConfig.java
+++ section-3/src/main/java/com/eazybutes/eazybytes/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // tell spring boot, we configuration some data in this class
@@ -18,6 +21,22 @@ public class ProjectSecurityConfig {
             .httpBasic(Customizer.withDefaults());
         
         return http.build();
+    }
+
+    // {noop} - NoOpPasswordEncoder store password with plain text
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withUsername("admin")
+            .password("{noop}admin")
+            .authorities("ADMIN")
+            .build();
+
+        UserDetails user = User.withUsername("user")
+            .password("{noop}user")
+            .authorities("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
