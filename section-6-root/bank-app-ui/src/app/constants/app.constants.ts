fda23cd856489280c1eca33af6549031da332192
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-13 09:46:15 +0700

    Update app.constants.ts

--- section-6-root/bank-app-ui/src/app/constants/app.constants.ts
+++ section-6-root/bank-app-ui/src/app/constants/app.constants.ts
@@ -1,10 +1,10 @@
 
 export const  AppConstants = {
-    CONTACT_API_URL : "/contact",
-    LOGIN_API_URL : "/user",
-    ACCOUNT_API_URL : "/myAccount",
+    CONTACT_API_URL : "/contacts",
+    LOGIN_API_URL : "/auths",
+    ACCOUNT_API_URL : "/accounts",
     BALANCE_API_URL : "/myBalance",
-    LOANS_API_URL : "/myLoans",
-    CARDS_API_URL : "/myCards",
+    LOANS_API_URL : "/loans",
+    CARDS_API_URL : "/cards",
     NOTICES_API_URL : "/notices"
 }
