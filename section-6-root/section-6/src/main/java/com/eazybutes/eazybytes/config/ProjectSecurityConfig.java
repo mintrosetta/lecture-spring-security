429013663833ef244e6356fcd692c65e2f8398ab
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-23 13:15:47 +0700

    enable cors config

--- section-6-root/section-6/src/main/java/com/eazybutes/eazybytes/config/ProjectSecurityConfig.java
+++ section-6-root/section-6/src/main/java/com/eazybutes/eazybytes/config/ProjectSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.eazybutes.eazybytes.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,26 +9,41 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration // tell spring boot, we configuration some data in this class
 public class ProjectSecurityConfig {
 
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        // csrf will block all post method
-        http.csrf(config -> config.disable())
-            .authorizeHttpRequests((auth) -> 
-                auth.requestMatchers("/accounts/**", "/balances/**", "/loans/**", "/cards/**").authenticated()
-                    .requestMatchers("/notices/**", "/contacts/**", "/auths/**").permitAll())
-            .formLogin(Customizer.withDefaults())
-            .httpBasic(Customizer.withDefaults());
-        
-        return http.build();
-    }
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		// csrf will block all post method
+		http.cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+				config.setAllowedMethods(Collections.singletonList("*"));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setMaxAge(3800L);
 
+				return config;
+			}
+		})).csrf(config -> config.disable())
+				.authorizeHttpRequests((auth) -> auth
+						.requestMatchers("/accounts/**", "/balances/**", "/loans/**", "/cards/**").authenticated()
+						.requestMatchers("/notices/**", "/contacts/**", "/auths/**").permitAll())
+				.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
